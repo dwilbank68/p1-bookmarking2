bd32dd66a6e93a6aa879272eecd204a42e3c971d
David Wilbanks <dwilbank@gmail.com>
2014-10-06 15:40:07 -0700

    parsing params

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -6,7 +6,12 @@ class IncomingController < ApplicationController
   def create
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
+    #puts "INCOMING PARAMS HERE: #{params}"
+    puts "*"*30
+    puts "SUBJECT: #{params["subject"]}"
+    puts "SENDER: #{params["sender"]}"
+    puts "LINK: #{params["stripped-text"]}"
+    puts "*"*30
 
     # You put the message-splitting and business
     # magic here.
