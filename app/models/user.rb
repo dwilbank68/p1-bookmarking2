191d562bc871e608c881865bfdd54280875ec6de
David Wilbanks <dwilbank@gmail.com>
2014-10-05 17:39:48 -0700

    uploading secrets.yml now

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   has_many  :topics
   has_many  :likes
-  has_many :bookmarks
+  has_many  :bookmarks
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
