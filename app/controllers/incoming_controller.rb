43729191570eb03a37fe7cd04c6c1ad1bc432c45
David Wilbanks <dwilbank@gmail.com>
2014-10-07 23:03:24 -0700

    email creates bookmark

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -13,10 +13,24 @@ class IncomingController < ApplicationController
     puts "LINK: #{params["stripped-text"]}"
     puts "*"*30
 
+    topic_name = params["subject"] || "Misc"
+    topic = Topic.find_or_create_by(:name => topic_name)
+    email_user = User.find_by_email(params["sender"])
+    bookmark = email_user.bookmarks.build(:url => params["stripped-text"], :topic => topic)
     # You put the message-splitting and business
     # magic here.
-
+    if bookmark.save
     # Assuming all went well.
-    head 200
+      # put an entry in the current_user's likes table for this bookmark
+      # email current_user back with a confirmation
+      head 200 # who is expecting or needing this head?
+      #email email_user back with a confirmation
+    else
+      #email email_user with failure notification
+    end
   end
 end
+
+# validates :url, :presence => true, :length => { :minimum =>2, :maximum => 255 }
+# validates :topic, presence: true
+# validates :user, presence: true
