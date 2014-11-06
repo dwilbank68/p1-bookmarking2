c099c4147e9ef55e811ee23b6572ba8fdfc68144
David Wilbanks <dwilbank@gmail.com>
2014-11-05 22:04:20 -0800

    ready to go

--- noko.rb
+++
@@ -1,7 +0,0 @@
-URL = "http://webthumb.bluga.net/easythumb.php?user=77017&url=www.consumerreports.com&hash=578f71e9c7f50bf11a53218c6739affa&size=medium&cache=30"
-
-require 'nokogiri' # gem install nokogiri
-require 'open-uri' # already part of your ruby install
-
-image = Nokogiri::HTML(open(URL))
-puts image.at_css('.decoded')
