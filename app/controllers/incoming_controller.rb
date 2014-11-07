60be62c623cbfa94749b7fe5ee4c699715429a3e
David Wilbanks <dwilbank@gmail.com>
2014-11-07 10:33:14 -0800

    cleaned up incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -9,61 +9,50 @@ class IncomingController < ApplicationController
   def create
     # https://github.com/embedly/embedly-ruby
     url = params["stripped-text"]
-    #url = "http://lifehacker.com";
-    #call_embedly(url)
 
-    embedly_key = '8837e2d5c8d14881a505d2fe96f40076';
-    embedly_user_agent = 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)';
-    embedly_api = Embedly::API.new :key => embedly_key, :user_agent => embedly_user_agent;
-    embedly_obj = embedly_api.extract( :url => url, :maxwidth => 280 );
-    embedly_obj = embedly_obj[0]
-
-    base = "http://api.embed.ly/1/oembed?url=";
-    # url = "http://popsci.com";
-    safe_url = CGI.escape(url);
-    res = Net::HTTP.get_response(URI.parse(base + safe_url));
-    # unless res.response.msg == "OK";
-    #   record.errors[:url] << 'url is invalid';
-    # end;
-    embedly_json = JSON.parse(res.body)
-    thumbnail_url = embedly_json["thumbnail_url"];
-    # puts thumbnail_url;
-
-
+    embedly_obj = get_embedly_obj(url)      ### For video embeds ###
+    embedly_json = get_embedly_json(url)    ### For thumbnail url ###
 
     topic_name = params["subject"] == "" ? "Misc" : params["subject"]
     topic = Topic.find_or_create_by(:name => topic_name)
-    color = topic.color_topic(topic.id)
+    color = topic.color_topic
     topic.update_attributes(color:color)
     email_user = User.find_by_email(params["sender"])
 
-    description = embedly_obj[:description]
-    title = embedly_obj[:title]
+    description, title = embedly_obj[:description], embedly_obj[:title]
     title = nil if title == description # if title and description come out identical, make title nil
+    thumbnail_url = embedly_json["thumbnail_url"]
     embed = embedly_obj[:media][:html]
-    bookmark = email_user.bookmarks.build(:url => url,
-                                          :topic => topic,
-                                          :description => description,
-                                          :title => title,
-                                          :thumbnail => thumbnail_url,
-                                          :embed => embed)
-                                          #:embed => '<img src="http://www.popsci.com/sites/popsci.com/files/styles/article_image_big/public/J-31%2001.jpg?itok=Np8wtPrB">')
-
+    bookmark = email_user.bookmarks.build(:url => url,                 :topic => topic,
+                                          :description => description, :title => title,
+                                          :thumbnail => thumbnail_url, :embed => embed)
 
     if bookmark.save
-        Like.create(:bookmark_id => bookmark.id,
-                    :user_id => email_user.id )
+        Like.create(:bookmark_id => bookmark.id, :user_id => email_user.id )
         UserMailer.confirmation_email(email_user, bookmark).deliver
         render :nothing => true
     else
         UserMailer.rejection_email(email_user, bookmark).deliver
         render :nothing => true
-   end
+    end
+
   end
 
 
+  def get_embedly_obj(url)
+    embedly_key = ENV['embedly_key'];
+    embedly_user_agent = 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)';
+    embedly_api = Embedly::API.new :key => embedly_key, :user_agent => embedly_user_agent;
+    embedly_obj = embedly_api.extract( :url => url, :maxwidth => 280 );
+    embedly_obj[0]
+  end
+
+  def get_embedly_json(url)
+    base = "http://api.embed.ly/1/oembed?url=";
+    safe_url = CGI.escape(url);
+    res = Net::HTTP.get_response(URI.parse(base + safe_url));
+    JSON.parse(res.body)
+  end
+
 
 end
-
-# "thumbnail_url":"([^"]*)"
-
