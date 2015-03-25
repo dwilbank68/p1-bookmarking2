da58cd801ee3bb208525a21289e7d7be289fe6e5
David Wilbanks <dwilbank@gmail.com>
2015-03-25 08:56:48 -0700

    Update bookmark.rb

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,23 +1,14 @@
 class Bookmark < ActiveRecord::Base
-  #include ActiveModel::Validations
 
   belongs_to  :topic
   has_many    :likes, dependent: :destroy
   belongs_to  :user
-  # validates :url, :presence => true, :length => { :minimum =>2, :maximum => 255 }
   validates_with UrlValidator# on: :url
-  #validates :url, :presence => true, :url => true
-  #validates :url, true # custom - see app/validators/url_validator.rb
   validates :topic, presence: true
   validates :user, presence: true
 
-
-  #default_scope {order('created_at DESC')}
-  #default_scope {order('topic.name DESC')}
-
   def liked?(current_user,bookmark)
     return false unless current_user
-    #Like.where(bookmark_id: id, user_id: current_user.id) ? "liked":""
     Like.where(bookmark_id: bookmark.id, user_id: current_user.id).count > 0 ? "liked-#{bookmark.topic.color}":""
   end
 
