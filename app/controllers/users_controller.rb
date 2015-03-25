ebdf4349298379820da94cef612f4c1a6d050772
David Wilbanks <dwilbank@gmail.com>
2015-03-25 09:03:23 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,9 @@ class UsersController < ApplicationController
     if params[:id] == current_user.id.to_s
       liked_bookmark_ids = current_user.likes.pluck(:bookmark_id)
       liked_bookmarks = Bookmark.where(id:liked_bookmark_ids)
-      # liked_topic_ids = liked_bookmarks.pluck(:topic_id)
+
       @current_user = current_user
-      # @liked_topics = Topic.where(id:liked_topic_ids).order('topics.name')
-        # this jumps straight to topics/_topic.html.erb instead of topics/show.html.erb
+
       @liked_bookmarks = liked_bookmarks.joins(:topic).order('topics.name').order('created_at DESC')
     else
        redirect_to current_user, notice: "Stick to your own page"
@@ -17,8 +16,3 @@ class UsersController < ApplicationController
   end
 
 end
-
-#.joins(:topic).order('topics.name')
-
-
-# @bookmarks = Bookmark.joins(:topic).order('topics.name').order('created_at DESC')
