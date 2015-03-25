3f8133c4b1dbc0ba0abc308e989dc84c56b8f918
David Wilbanks <dwilbank@gmail.com>
2015-03-25 09:02:28 -0700

    Update url_validator.rb

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,23 +1,6 @@
 require 'net/http'
 require 'net/smtp'
 
-# class UrlValidator < ActiveModel::Validator
-#   def validate(record)
-#     res = Net::HTTP.get_response(URI.parse(record.url))
-#     # unless ["OK", "Object moved", "Moved Permanently", "Found"].include? res.response.msg
-#     unless ["200", "301", "302"].include? res.code
-#       # record.errors[:url] << "#{record.url} invalid - #{res.response.msg}"
-#       record.errors[:url] << "#{record.url} invalid - #{res.code}"
-#     end
-#   end
-# end
-
-# require 'net/http';
-# require 'net/smtp';
-# res = Net::HTTP.get_response(URI.parse("http://www.lifehacker.com"));
-# res.response.msg;
-###### delete everything below. it is just for question purposes
-
 class UrlValidator < ActiveModel::Validator
   def validate(record)
 
